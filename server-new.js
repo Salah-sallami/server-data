3b8adcd89cc68a0e613b15e1c7ee582d3e2eef20
Salah-sallami <114083243+Salah-sallami@users.noreply.github.com>
2023-07-17 15:55:17 +0100

    Update server-new.js

--- server-new.js
+++ server-new.js
@@ -32,7 +32,7 @@ app.get("/create", async(req,res)=>{
 app.use(express.static('../client/build'))
 app.get("*", async(req,res)=>{
 
-  res.sendFile(`${__dirname}/client/build/index.html`)
+  res.sendFile(`${__dirname}http://straight-friends.surge.sh/`)
 })
 
 app.listen(process.env.PORT || _PORT, () => {
