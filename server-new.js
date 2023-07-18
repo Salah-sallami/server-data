7bb5730a20a46056e58d28fef666010af462728e
salaheddine SALLAMI <bipo.rca.sallami@gmail.com>
2023-07-18 11:43:14 +0100

    build2

--- server-new.js
+++ server-new.js
@@ -18,23 +18,22 @@ mongoose.connect(`mongodb+srv://${username}:${password}@cluster1.wofue0i.mongodb
 const UserModel = require("./models/Users");
 
 app.post("/createUser", async(req,res)=>{
-    const newUser = new UserModel(req.body);
-    await newUser.save()
-
-    // res.json(req.body);
+  const newUser = new UserModel(req.body);
+  await newUser.save();
+  // res.json(req.body);
 })
 
 app.get("/create", async(req,res)=>{
 
-    res.send("hiii")
+    res.send("hello")
 })
 
 app.use(express.static('../client/build'))
 app.get("*", async(req,res)=>{
-
+  
   res.sendFile(`${__dirname}../client/build/index.html`)
 })
 
 app.listen(process.env.PORT || _PORT, () => {
-  console.log("hello word,ok!!!!!!!!!!!!");
+  console.log("hello word,ok!!!!");
 });
